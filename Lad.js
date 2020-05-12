568fb784ee45b7d7dd0f2e90ca8b1f8bff886b03
Camto <www.ben.pr@gmail.com>
2020-05-12 02:18:10 -0400

    Find timeout channel.

--- Lad.js
+++ Lad.js
@@ -4,9 +4,12 @@ var discord = require("discord.js");
 var auth = require("./auth.json");
 
 var client = new discord.Client();
+var timeout_channel;
 
 client.on("ready", () => {
 	console.log(`${client.user.tag} is logged in!`);
+	timeout_channel = client.channels.fetch("709639641568903179")
+		.then(channel => {timeout_channel = channel;});
 });
 
 client.on("voiceStateUpdate", (old_st, new_st) => {
