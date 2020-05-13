02f3cea905444cd9beccf574b7282bfa5983e9d3
Camto <www.ben.pr@gmail.com>
2020-05-13 12:04:14 -0400

    Help now shows itself and no more `L.` prefix, only lower.

--- Lad.py
+++ Lad.py
@@ -24,12 +24,14 @@ async def on_ready():
 	await client.change_presence(status = discord.Status.online, activity = activity)
 	print("Logged in")
 
+# Show command descriptions.
 @client.command()
 async def help(ctx):
 	await ctx.send(embed = discord.Embed(
 		title = "Commands",
-		description = "This bot's prefix is ``l.`` or ``L.``",
+		description = "This bot's prefix is ``l.``",
 		color = embed_color)
+		.add_field(name = "help", value = "Show this message.", inline = False)
 		.add_field(name = "bible", value = "Returns a random bible verse.", inline = False)
 		.add_field(name = "ping", value = "Responds with pong.", inline = False)
 		.add_field(name = "say", value = "Make the bot say something.", inline = False)
@@ -60,9 +62,9 @@ async def on_message(msg):
 	if msg.author.name != "Lad":
 		content = msg.content
 		
-		if content.lower().startswith("l."):
+		if content.startswith("l."):
 			# The say command is actually here.
-			if content.lower().startswith("l.say"):
+			if content.startswith("l.say"):
 				return await msg.channel.send(content[5:].lstrip())
 			else:
 				return await client.process_commands(msg)
