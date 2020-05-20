61e15cef55c52a1393b6d6b4d096af457a9f27f5
Jan-Paul <JanPaul.Ramos@protonmail.com>
2020-05-20 05:26:09 -0400

    Cogs test
    testing out cogs

---
+++ Cogs/cogstest.py
@@ -0,0 +1,14 @@
+# Cogs Test
+import discord
+from discord.ext import commands
+
+class Test(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send("Cogs test")
+
+def setup(client):
+    client.add_cog(Test(client))
