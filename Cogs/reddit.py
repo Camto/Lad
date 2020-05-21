3403d42635fd9b566691717abead3a97caee8a27
Camto <www.ben.pr@gmail.com>
2020-05-20 23:35:27 -0400

    Move `l.reddit`.

---
+++ Cogs/reddit.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+
+import json
+import requests
+import urllib.parse
+
+import utils
+
+# Get Reddit posts and users.
+class Reddit(commands.Cog):
+	def __init__(self, client):
+		self.client = client
+	
+	@commands.command()
+	async def reddit(self, ctx, *args):
+		if len(args) >= 1:
+			post = json.loads(requests.get(
+				f"https://www.reddit.com/r/{urllib.parse.quote(args[0], safe = '')}/random.json",
+				headers = {"User-agent": "Ladbot"}).text)
+			if "error" not in post:
+				post = post[0]["data"]["children"][0]["data"]
+				msg = await ctx.send(embed = post_to_embed(post))
+			else:
+				print(post["error"])
+
+def post_to_embed(post):
+	embed = (discord.Embed(
+		description = post["selftext"],
+		color = utils.embed_color)
+		.set_author(
+			name = post["title"],
+			icon_url = utils.icons["reddit"],
+			url = f'https://www.reddit.com{post["permalink"]}'))
+	
+	return embed
+
+def setup(client):
+	client.add_cog(Reddit(client))
--- Lad.py
+++ Lad.py
@@ -6,9 +6,7 @@ import os
 import random
 import json
 import asyncio
-import urllib.parse
 
-import requests
 import aiosqlite
 from fuzzywuzzy import process
 from fuzzywuzzy import fuzz
@@ -130,19 +128,6 @@ async def bible(ctx, *args):
 	else:
 		await ctx.send(embed = command_disabled)
 
-# Get Reddit posts and users.
-@client.command()
-async def reddit(ctx, *args):
-	if len(args) >= 1:
-		post = json.loads(requests.get(
-			f"https://www.reddit.com/r/{urllib.parse.quote(args[0], safe = '')}/random.json",
-			headers = {"User-agent": "Ladbot"}).text)
-		if "error" not in post:
-			post = post[0]["data"]["children"][0]["data"]
-			msg = await ctx.send(embed = post_to_embed(post))
-		else:
-			print(post["error"])
-
 # The autoresponse system and relegating commands.
 @client.event
 async def on_message(msg):
@@ -172,17 +157,6 @@ async def on_message(msg):
 				for keyword in pair["keywords"]:
 					if keyword in text:
 						return await msg.channel.send(random.choice(pair["responses"]))
-
-def post_to_embed(post):
-	embed = (discord.Embed(
-		description = post["selftext"],
-		color = utils.embed_color)
-		.set_author(
-			name = post["title"],
-			icon_url = utils.icons["reddit"],
-			url = f'https://www.reddit.com{post["permalink"]}'))
-	
-	return embed
 
 async def start_bot():
 	# Start up settings database.
