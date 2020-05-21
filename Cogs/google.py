2bd9d9375a1c2277611bdcaebf81351e5cb6044d
Jan-Paul <JanPaul.Ramos@protonmail.com>
2020-05-21 06:54:16 -0400

    indentation moment
    fixing google command

--- Cogs/google.py
+++ Cogs/google.py
@@ -1,27 +1,27 @@
 import discord
 from discord.ext import commands
 
-import google
+from google import google
 import utils
 
 class Google(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-@commands.command()
-async def search(self, ctx, *args):
-    num_page = 1
-    if len(args) >= 1:
+    @commands.command()
+    async def search(self, ctx, *args):
+        num_page = 1
         search_results = google.search(args[0], num_page)
-        for result in search_results:
-            await ctx.send(embed = discord.Embed(
-                title = "Google Search",
-                description = result.description,
+        if len(args) >= 1:
+            for result in search_results:
+                await ctx.send(embed = discord.Embed(
+                    title = "Google Search",
+                    description = result.description,
+                    color = utils.embed_color))
+        else:
+            await ctx.send(embed = discord.Embed( 
+                description = "No query given.",
                 color = utils.embed_color))
-    else:
-        await ctx.send(embed = discord.Embed( 
-            description = "No query given.",
-            color = utils.embed_color))
 
 def setup(client):
     client.add_cog(Google(client))
