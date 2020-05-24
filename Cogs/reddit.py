4f91b0c4b1b2be7074e457b94f382712656c323a
Camto <www.ben.pr@gmail.com>
2020-05-24 01:25:46 -0400

    Add reload functionality to `l.reddit`

--- Cogs/reddit.py
+++ Cogs/reddit.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
 
+import asyncio
 import json
 import requests
 import urllib.parse
 import utils
+
+reload_emoji = "\U0001f504"
 
 # Get Reddit posts and users.
 class Reddit(commands.Cog):
@@ -14,16 +17,26 @@ class Reddit(commands.Cog):
 	@commands.command()
 	async def reddit(self, ctx, *args):
 		if len(args) >= 1:
-			post = json.loads(requests.get(
-				f"https://www.reddit.com/r/{urllib.parse.quote(args[0], safe = '')}/random.json",
-				headers = {"User-agent": "Ladbot"}).text)
-			if "error" not in post:
-				if isinstance(post, list):
-					post = post[0]
-				post = post["data"]["children"][0]["data"]
-				msg = await ctx.send(embed = post_to_embed(post))
-			else:
-				print(post["error"])
+			sub = args[0]
+			
+			msg = await ctx.send(embed =
+				post_to_embed(get_random_post(sub)))
+			
+			await msg.add_reaction(reload_emoji)
+			
+			while True:
+				try:
+					reaction, user = await self.client.wait_for(
+						"reaction_add",
+						timeout = 60,
+						check = lambda reaction, user:
+							user == ctx.author and str(reaction.emoji) == reload_emoji)
+				except asyncio.TimeoutError:
+					break
+				else:
+					await msg.remove_reaction(reload_emoji, user)
+					await msg.edit(embed =
+						post_to_embed(get_random_post(sub)))
 
 def post_to_embed(post):
 	embed = (discord.Embed(
@@ -44,6 +57,23 @@ def post_to_embed(post):
 				embed.set_thumbnail(url = post["thumbnail"])
 	
 	return embed
+
+def get_random_post(sub):
+	post = json.loads(requests.get(
+		f"https://www.reddit.com/r/{urllib.parse.quote(sub, safe = '')}/random.json",
+		headers = {"User-agent": "Ladbot"}).text)
+	if "error" not in post:
+		if isinstance(post, list):
+			post = post[0]
+		return post["data"]["children"][0]["data"]
+	else:
+		print(post["error"])
+		return {
+			"title": "Error",
+			"selftext": "Failed to get post.",
+			"is_self": True,
+			"permalink": ""
+		}
 
 def setup(client):
 	client.add_cog(Reddit(client))
