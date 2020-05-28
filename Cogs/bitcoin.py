3cf13523e04567ea0fdf09e3d7a0fbd59ac84654
Jan-Paul <JanPaul.Ramos@protonmail.com>
2020-05-28 13:25:29 -0400

    Bitcoin update

--- Cogs/bitcoin.py
+++ Cogs/bitcoin.py
@@ -11,34 +11,29 @@ class Bitcoin(commands.Cog):
 
     @commands.command()
     async def bitcoin(self, ctx, *cmd):
-	    url = "https://api.coindesk.com/v1/bpi/currentprice.json"
-	    async with aiohttp.ClientSession() as session:  # Async HTTP request
-		    raw_response = await session.get(url)
-		    response = await raw_response.text()
-		    response = json.loads(response)
-		    currencies = ["USD","EUR","GBP"]
-		    if len(cmd) == 0:
-			    await ctx.send(embed = discord.Embed(
-				    title = ':information_source: Info',
-				    description = f"Current list of currencies", 
-				    color=utils.embed_color)
-				    .add_field(name = "USD", value = "United States Dollar", inline = False)
-				    .add_field(name = "EUR", value = "Euro", inline = False)
-				    .add_field(name = "GBP", value = "British Pound Sterling", inline = False))
-		    elif cmd[0].upper() in currencies:
-			    currency = cmd[0]
-			    await ctx.send(embed = discord.Embed(
-				    title = ':information_source: Info',
-				    description = f"Bitcoin price is: ${response['bpi'][currency.upper()]['rate']} USD", 
-				    color=utils.embed_color)
-				    .set_footer(text = f"Requested by {ctx.message.author}."))
-		    else:
-			    await ctx.send(embed = discord.Embed(
-				    title = ':anger: Error: Currency not found.', 
-				    color=utils.embed_color)
-				    .add_field(name = "USD", value = "United States Dollar", inline = False)
-				    .add_field(name = "EUR", value = "Euro", inline = False)
-				    .add_field(name = "GBP", value = "British Pound Sterling", inline = False))
+	    if len(cmd) == 0:
+        request = requests.get("https://api.coindesk.com/v1/bpi/currentprice/USD.json")
+        info = json.loads(request.text)
+        await ctx.send(embed = discord.Embed(
+            title = ':information_source: Info',
+            description = f"Bitcoin price is: {info['bpi']['USD']['rate']} USD", 
+            color=COLOR)
+            .set_footer(text = f"Requested by {ctx.message.author}."))
+    else:
+        currency = cmd[0].upper()
+        request = requests.get(f"https://api.coindesk.com/v1/bpi/currentprice/{urllib.parse.quote(currency, safe = '')}.json")
+        if request.status_code == 404:
+            await ctx.send(embed = discord.Embed(
+                title = ':anger: Error, currency not found',
+                description = "Current list of currencies: [Official Website](https://www.coindesk.com/price/bitcoin)", 
+                color=COLOR))
+        elif request.status_code == 200:
+            info = json.loads(request.text)
+            await ctx.send(embed = discord.Embed(
+                title = ':information_source: Info',
+                description = f"Bitcoin price is: {info['bpi'][currency]['rate']} {currency}", 
+                color=COLOR)
+                .set_footer(text = f"Requested by {ctx.message.author}."))
 
 def setup(client):
     client.add_cog(Bitcoin(client))
