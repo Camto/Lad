d32801328ad7f366a5285d7901746f40b1fb0c25
Camto <www.ben.pr@gmail.com>
2020-07-02 03:53:28 -0400

    Add the magical, mystical menu function!
    It shall handle all reaction menus.

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+import asyncio
 import yaml
 
 lad_id = 709644595104972890
@@ -27,3 +28,41 @@ def get_setting(guild_id, option):
 		return settings[guild_id][option]
 	else:
 		return options[option]["default"]
+
+async def menu(client, ctx, gen_):
+	gen = gen_(ctx.author)
+	
+	embed, reactions = await gen.__anext__()
+	msg = await ctx.send(embed = embed)
+	for reaction in reactions:
+		await msg.add_reaction(reaction)
+	
+	while True:
+		try:
+			reaction, user = await client.wait_for(
+				"reaction_add",
+				timeout = 60,
+				check = lambda reaction, user:
+					reaction.message.id == msg.id and
+					user == ctx.author and
+					str(reaction.emoji) in reactions)
+		except asyncio.TimeoutError:
+			break
+		else:
+			await gen.asend(reaction.emoji)
+			embed, new_reactions = await gen.__anext__()
+			
+			await msg.edit(embed = embed)
+			await msg.remove_reaction(str(reaction.emoji), user)
+			
+			if new_reactions != None:
+				for reaction in reactions:
+					await msg.remove_reaction(reaction, client.user)
+				for reaction in new_reactions:
+					await msg.add_reaction(reaction)
+				reactions = new_reactions
+	
+	gen.aclose()
+
+class menus():
+	menu = menu
