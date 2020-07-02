00c9fe050de73869b0cfcac25df79e0e9d48b3b1
Camto <www.ben.pr@gmail.com>
2020-07-02 04:16:32 -0400

    Make `l.dino` use the new and fancy `menus.reload`.

--- Cogs/dino.py
+++ Cogs/dino.py
@@ -20,41 +20,17 @@ class Dino(commands.Cog):
 	@commands.command()
 	async def dino(self, ctx, *args):
 		if utils.get_setting(ctx.guild.id, "dino"):
-			if len(args) == 0:
-				dino = random.choice(dino_names)
-			else:
-				dino = process.extractOne(args[0], dino_names)[0]
-			
 			if len(args) != 0:
+				dino = process.extractOne(args[0], dino_names)[0]
 				await ctx.send(embed = dino_to_embed(dino))
 			else:
-				reload_amount = 1
-				
-				msg = await ctx.send(embed =
-					dino_to_embed(dino)
-					.set_footer(text = f"#{reload_amount}"))
-				
-				await msg.add_reaction(reload_emoji)
-				
-				while True:
-					try:
-						reaction, user = await self.client.wait_for(
-							"reaction_add",
-							timeout = 60,
-							check = lambda reaction, user:
-								reaction.message.id == msg.id and
-								user == ctx.author and
-								str(reaction.emoji) == reload_emoji)
-					except asyncio.TimeoutError:
-						break
-					else:
-						reload_amount += 1
-						await msg.remove_reaction(reload_emoji, user)
-						await msg.edit(embed =
-							dino_to_embed(random.choice(dino_names))
-							.set_footer(text = f"#{reload_amount}"))
+				await utils.menus.reload(self.client, ctx, dino_menu)
 		else:
 			await ctx.send(embed = utils.command_disabled)
+
+async def dino_menu(_):
+	while True:
+		yield dino_to_embed(random.choice(dino_names))
 
 def dino_to_embed(dino):
 	return (discord.Embed(
