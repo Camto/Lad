9b82e933e64e189c3a4dd9758cfb4ccc5f185673
Camto <www.ben.pr@gmail.com>
2020-07-03 01:45:13 -0400

    Change `l.bible` to use `menus.reload`.

--- Cogs/bible.py
+++ Cogs/bible.py
@@ -19,44 +19,20 @@ class Bible(commands.Cog):
 	@commands.command()
 	async def bible(self, ctx, *args):
 		if utils.get_setting(ctx.guild.id, "bible"):
-			if len(args) == 0:
-				quote = random.choice(quotes)
-			else:
+			if len(args) != 0:
 				quote = process.extractOne(
 					args[0],
 					quotes,
 					scorer = fuzz.partial_token_sort_ratio)[0]
-
-			if len(args) != 0:
 				await ctx.send(embed = bible_to_embed(quote))
 			else:
-				reload_amount = 1
-
-				msg = await ctx.send(embed = 
-					bible_to_embed(quote)
-					.set_footer(text = f"#{reload_amount}"))
-
-				await msg.add_reaction(reload_emoji)
-
-				while True:
-					try:
-						reaction, user = await self.client.wait_for(
-							"reaction_add",
-							timeout = 60,
-							check = lambda reaction, user:
-								reaction.message.id == msg.id and
-								user == ctx.author and
-								str(reaction.emoji) == reload_emoji)
-					except asyncio.TimeoutError:
-						break
-					else:
-						reload_amount += 1
-						await msg.remove_reaction(reload_emoji, user)
-						await msg.edit(embed = 
-							bible_to_embed(random.choice(quotes))
-							.set_footer(text = f"#{reload_amount}"))
+				await utils.menus.reload(self.client, ctx, bible_menu)
 		else:
 			await ctx.send(embed = utils.command_disabled)
+
+async def bible_menu(_):
+	while True:
+		yield bible_to_embed(random.choice(quotes))
 
 def bible_to_embed(quote):
 	return (discord.Embed(
