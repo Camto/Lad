92ed9864b71994e1facfe62b38c455cb052911fc
jpVinnie <janpaul.ramos@protonmail.com>
2020-07-07 19:59:38 -0400

    I am the smartest person alive

--- Cogs/convert.py
+++ Cogs/convert.py
@@ -30,33 +30,33 @@ class Convert(commands.Cog):
             temp_file = ''.join(random.choice(string.ascii_lowercase)
                                 for i in range(10))
 
-        try:
-            r = requests.get(url, allow_redirects=True)
+            try:
+                r = requests.get(url, allow_redirects=True)
 
-            open(f'{temp_file}', 'wb').write(r.content)
-            # await ctx.send(embed=discord.Embed(
-            #    title=":white_check_mark: File Downloaded",
-            #    color=utils.embed_color)
-            #    .set_footer(text=f'Requested by {ctx.message.author}.'))
-        except:
-            await ctx.send(embed=discord.Embed(
-                title=":bangbang: Error while downloading file | Try another link",
-                color=utils.embed_color)
-                .set_footer(text=f'Requested by {ctx.message.author}.'))
-        else:
-            im = Image.open(temp_file)
-            im.save(f'{temp_file}.{extension}', quality=100)
-            nfile = discord.File(
-                f'{temp_file}.{extension}', filename=f"Converted File.{extension}")
-            await ctx.send(file=nfile, embed=discord.Embed(
-                title=":white_check_mark: File Successfully Converted!",
-                color=utils.embed_color)
-                .set_footer(text=f'Requested by {ctx.message.author}.'))
-        finally:
-            if os.path.isfile(f'{temp_file}.{extension}'):
-                os.remove(f'{temp_file}.{extension}')
-            if os.path.isfile(temp_file):
-                os.remove(temp_file)
+                open(f'{temp_file}', 'wb').write(r.content)
+                # await ctx.send(embed=discord.Embed(
+                #    title=":white_check_mark: File Downloaded",
+                #    color=utils.embed_color)
+                #    .set_footer(text=f'Requested by {ctx.message.author}.'))
+            except:
+                await ctx.send(embed=discord.Embed(
+                    title=":bangbang: Error while downloading file | Try another link",
+                    color=utils.embed_color)
+                    .set_footer(text=f'Requested by {ctx.message.author}.'))
+            else:
+                im = Image.open(temp_file)
+                im.save(f'{temp_file}.{extension}', quality=100)
+                nfile = discord.File(
+                    f'{temp_file}.{extension}', filename=f"Converted File.{extension}")
+                await ctx.send(file=nfile, embed=discord.Embed(
+                    title=":white_check_mark: File Successfully Converted!",
+                    color=utils.embed_color)
+                    .set_footer(text=f'Requested by {ctx.message.author}.'))
+            finally:
+                if os.path.isfile(f'{temp_file}.{extension}'):
+                    os.remove(f'{temp_file}.{extension}')
+                if os.path.isfile(temp_file):
+                    os.remove(temp_file)
 
 
 def setup(client):
