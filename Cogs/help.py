ffbfb28efa1785ab654648abc77c33f8fcf74196
jpVinnie <janpaul.ramos@protonmail.com>
2020-07-09 14:22:37 -0400

    remove search from help, adding convert

--- Cogs/help.py
+++ Cogs/help.py
@@ -24,8 +24,8 @@ class Help(commands.Cog):
 			.add_field(name = "say", value = "Make the bot say something.", inline = False)
 			.add_field(name = "ascii", value = "Returns your message in ASCII art!", inline = False)
 			.add_field(name = "reddit", value = "Sends a link to a random reddit post.", inline = False)
-			.add_field(name = "search", value = "Get a list of Google search results.", inline = False)
 			.add_field(name = "bible", value = "Returns a random bible verse. For verses do l.help bible", inline = False)
+			.add_fiel(name = "convert", value = "Convert a file by copying it's address and using 'l.convert <extension> <file>", inline = False)
 			.add_field(name = "bitcoin", value = "Get the current price of bitcoin with a wide range of currencies to choose from", inline = False)
 			.add_field(name = "dino", value = "Use `l.dino` for a random dinosaur, `l.dino <dinosaur name here>` to find the dinosaur with that name.", inline = False))
 		elif cmd[0] == "bible":
