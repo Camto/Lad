196d744faf9e25667507284f7e63bd2a20b323c6
jpVinnie <janpaul.ramos@protonmail.com>
2020-07-13 09:45:24 -0400

    optimizing roll command

--- Cogs/roll.py
+++ Cogs/roll.py
@@ -11,31 +11,39 @@ class Roll(commands.Cog):
 
     @commands.command()
     async def roll(self, ctx, *cmd):
-        if len(cmd) == 0:
-            await ctx.send(embed=discord.Embed(
-                description="Roll using DnD format (Example: 1d20)",
-                color=utils.embed_color)
-                .set_footer(text=f'Requested by {ctx.message.author}.'))
-
-        def d(sides):
-            return randint(1, sides)
-
-        def roll(n, sides):
-            return tuple(d(sides) for _ in range(n))
-
-        amount, side = cmd[0].split("d")
-
-        dice = roll(int(amount), int(side))
-
         try:
-            await ctx.send(embed=discord.Embed(
-                title=":game_die: Rolling dice...",
-                description=f"Your destiny is... ``{dice, sum(dice)}``",
-                color=utils.embed_color)
-                .set_footer(text=f'Requested by {ctx.message.author}.'))
+
+            def d(sides):
+                return randint(1, sides)
+
+            def roll(n, sides):
+                return tuple(d(sides) for _ in range(n))
+
+            amount, side = cmd[0].split("d")
+
+            dice = roll(int(amount), int(side))
+
+            try:
+                if sum(dice) >= 250000:
+                    await ctx.send(embed=discord.Embed(
+                        description="Your input is too big to calculate | > 250,000",
+                        color=utils.embed_color)
+                        .set_footer(text=f'Requested by {ctx.message.author}.'))
+                else:
+                    await ctx.send(embed=discord.Embed(
+                        title=":game_die: Rolling dice...",
+                        description=f"Your destiny is... ``{dice, sum(dice)}``",
+                        color=utils.embed_color)
+                        .set_footer(text=f'Requested by {ctx.message.author}.'))
+            except:
+                await ctx.send(embed=discord.Embed(
+                    description="Your input is too big to calculate",
+                    color=utils.embed_color)
+                    .set_footer(text=f'Requested by {ctx.message.author}.'))
         except:
+
             await ctx.send(embed=discord.Embed(
-                description="Your input is too big to calculate",
+                description="Roll using DnD rules (Example: 1d20)",
                 color=utils.embed_color)
                 .set_footer(text=f'Requested by {ctx.message.author}.'))
 
