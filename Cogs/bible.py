434adfac815eabaa0f87952302494e912db9b47e
Camto <www.ben.pr@gmail.com>
2020-11-06 01:34:56 -0400

    `l.bible` with *every* King James quote.

--- Cogs/bible.py
+++ Cogs/bible.py
@@ -6,7 +6,7 @@ import random
 
 from fuzzywuzzy import fuzz, process
 
-quotes = utils.get_yaml("bible quotes")
+bible = utils.get_yaml("bible")
 
 # Starting the bible study.
 class Bible(commands.Cog):
@@ -16,12 +16,12 @@ class Bible(commands.Cog):
 	@commands.command()
 	async def bible(self, ctx, *args):
 		if utils.get_setting(ctx.guild.id, "bible"):
-			if len(args) != 0:
-				quote = process.extractOne(
-					args[0],
-					quotes,
-					scorer = fuzz.partial_token_sort_ratio)[0]
-				await ctx.send(embed = bible_to_embed(quote))
+			if len(args) > 0:
+				pass#quote = process.extractOne(
+				#	args[0],
+				#	quotes,
+				#	scorer = fuzz.partial_token_sort_ratio)[0]
+				#await ctx.send(embed = bible_to_embed(quote))
 			else:
 				await utils.menus.reload(self.client, ctx, bible_menu)
 		else:
@@ -29,14 +29,18 @@ class Bible(commands.Cog):
 
 async def bible_menu(_):
 	while True:
-		yield bible_to_embed(random.choice(quotes))
+		book = random.choice(bible)
+		chapters = book['chapters']
+		chapter = random.randrange(len(chapters))
+		verse = random.randrange(len(chapters[chapter]))
+		yield bible_to_embed(chapters[chapter][verse], f"{book['name']} {chapter+1}:{verse+1}")
 
-def bible_to_embed(quote):
+def bible_to_embed(quote, loc):
 	return (discord.Embed(
-		description = quote["text"],
+		description = quote,
 		color = utils.embed_color)
 		.set_author(
-			name = quote["location"],
+			name = loc,
 			icon_url = utils.icons["bible"]))
 
 def setup(client):
