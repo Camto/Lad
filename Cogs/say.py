b40d607ec568439d195a8090c12a942d4269d10d
Camto <www.ben.pr@gmail.com>
2020-11-06 01:36:23 -0400

    Fix dumbass `l.say` comment.

--- Cogs/say.py
+++ Cogs/say.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import utils
 
-# Process dino related requests.
+# Make the bot say anything.
 class Say(commands.Cog):
 	def __init__(self, client):
 		self.client = client
