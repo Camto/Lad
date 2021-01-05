ad553e71ae28cbc7004e54d91d523d328a27eabf
Camto <www.ben.pr@gmail.com>
2021-01-04 23:23:19 -0400

    Add evaling to console.

--- Cogs/console.py
+++ Cogs/console.py
@@ -47,18 +47,24 @@ lad_script = lark.Lark(r"""
 	instrs: WS? (instr WS?)*
 	
 	?instr: WORD -> name
+		| eval
 		| path
 		| mention
 		| INT -> int
 		| FLOAT -> float
+		| word_string
 		| ESCAPED_STRING -> string
 		| store
 		| func
+	
+	eval: /```([^`]|`(?!``))*```/
 	
 	?path: /\.+(\/[^ \n]+)?/ -> plain_path
 		| "/" ESCAPED_STRING -> quoted_path
 	
 	mention: "@" ESCAPED_STRING
+	
+	word_string: "'" WORD
 	
 	store: "->" WORD
 	
@@ -76,21 +82,23 @@ class Types(enum.Enum):
 		actions,
 		msg, var, instrs,
 		name, int, float, string, path, mention,
-		store, func
-	) = range(12)
+		store, eval, func
+	) = range(13)
 
 class Lad_Script_Transformer(lark.Transformer):
 	start = list
 	msg = lambda self, m: {"type": Types.msg, "msg": m[0][1:]}
-	var = lambda self, v: {"type": Types.var, "name": v[0][0:].lower().replace("_", ""), "def": [v[3]]}
+	var = lambda self, v: {"type": Types.var, "name": v[0].lower().replace("_", ""), "def": [v[3]]}
 	instrs = lambda self, is_: {"type": Types.instrs, "instrs": list(filter(lambda i: i is not None, is_))}
 	name = lambda self, n: {"type": Types.name, "name": n[0].lower().replace("_", "")}
 	int = lambda self, n: {"type": Types.int, "int": int(n[0])}
 	float = lambda self, n: {"type": Types.float, "float": float(n[0])}
-	string = lambda self, s: {"type": Types.string, "string": eval(s[0].replace("\n", "\\n"))}
-	plain_path = lambda self, p: {"type": Types.path, "path": p[0][0:]}
+	string = lambda self, s: {"type": Types.string, "string": s[0]}
+	eval = lambda self, e: {"type": Types.eval, "eval": e[0][3:-3]}
+	plain_path = lambda self, p: {"type": Types.path, "path": p[0]}
 	quoted_path = lambda self, p: {"type": Types.path, "path": p[0][1:-1]}
 	mention = lambda self, m: {"type": Types.mention, "mention": p[0][1:-1]}
+	word_string = lambda self, s: {"type": Types.string, "string": s[0]}
 	store = lambda self, s: {"type": Types.store, "name": s[0].lower().replace("_", "")}
 	func = lambda self, f: {"type": Types.func, "body": f[0]}
 	WS = lambda self, _: None
@@ -123,6 +131,8 @@ async def run(client, actions, locals):
 			for instr in action["instrs"]:
 				if instr["type"] in [Types.int, Types.float, Types.string, Types.path, Types.mention, Types.func]:
 					st.append(instr)
+				elif instr["type"] == Types.eval:
+					eval(instr["eval"])
 
 def setup(client):
 	client.add_cog(Console(client))
