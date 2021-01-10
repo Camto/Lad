ee4afbda420a01302384065cfdbd0e5f4f22a409
Camto <www.ben.pr@gmail.com>
2021-01-09 21:36:40 -0400

    Remove `inline` because stupid bug.

--- Cogs/server.py
+++ Cogs/server.py
@@ -13,30 +13,31 @@ class Server(commands.Cog):
 			color = utils.embed_color)
 			.set_thumbnail(url = ctx.guild.icon_url))
 		
-		statuses = [
-			len(list(filter(lambda m: str(m.status) == "online", ctx.guild.members))),
-			len(list(filter(lambda m: str(m.status) == "idle", ctx.guild.members))),
-			len(list(filter(lambda m: str(m.status) == "dnd", ctx.guild.members))),
-			len(list(filter(lambda m: str(m.status) == "offline", ctx.guild.members)))]
+		#statuses = [
+		#	len(list(filter(lambda m: str(m.status) == "online", ctx.guild.members))),
+		#	len(list(filter(lambda m: str(m.status) == "idle", ctx.guild.members))),
+		#	len(list(filter(lambda m: str(m.status) == "dnd", ctx.guild.members))),
+		#	len(list(filter(lambda m: str(m.status) == "offline", ctx.guild.members)))]
 		
 		fields = [
-			("ID", ctx.guild.id, True),
-			("Owner", ctx.guild.owner, True),
-			("Region", ctx.guild.region, True),
-			("Created at", ctx.guild.created_at.strftime("%d/%m/%Y %H:%M:%S"), True),
-			("Members", len(ctx.guild.members), True),
-			("Humans", len(list(filter(lambda m: not m.bot, ctx.guild.members))), True),
-			("Bots", len(list(filter(lambda m: m.bot, ctx.guild.members))), True),
-			("Banned members", len(await ctx.guild.bans()), True),
-			("Statuses", f"🟢 {statuses[0]} 🟠 {statuses[1]} 🔴 {statuses[2]} ⚪ {statuses[3]}", True),
-			("Text channels", len(ctx.guild.text_channels), True),
-			("Voice channels", len(ctx.guild.voice_channels), True),
-			("Categories", len(ctx.guild.categories), True),
-			("Roles", len(ctx.guild.roles), True),
-			("Invites", len(await ctx.guild.invites()), True),
-			("\u200b", "\u200b", True)]
+			("ID", ctx.guild.id),
+			("Owner", ctx.guild.owner),
+			("Region", ctx.guild.region),
+			("Created at", ctx.guild.created_at.strftime("%d/%m/%Y %H:%M:%S")),
+			("Members", len(ctx.guild.members)),
+			("Humans", len(list(filter(lambda m: not m.bot, ctx.guild.members)))),
+			("Bots", len(list(filter(lambda m: m.bot, ctx.guild.members)))),
+			("Banned members", len(await ctx.guild.bans())),
+			# ("Statuses", f"🟢 {statuses[0]} 🟠 {statuses[1]} 🔴 {statuses[2]} ⚪ {statuses[3]}"),
+			("Text channels", len(ctx.guild.text_channels)),
+			("Voice channels", len(ctx.guild.voice_channels)),
+			("Categories", len(ctx.guild.categories)),
+			("Roles", len(ctx.guild.roles)),
+			("Invites", len(await ctx.guild.invites())),
+			("\u200b", "\u200b")]
 		
 		for name, value in fields:
+			print(f"Did field {name}")
 			embed.add_field(name = name, value = value, inline = True)
 		
 		await ctx.send(embed = embed)
