42b64e30ba5a2142ce82ab6715a2c9f68d2ca296
Camto <www.ben.pr@gmail.com>
2021-01-24 02:18:11 -0400

    Funny `l.feedback` command.

---
+++ Cogs/feedback.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import utils
+
+# Set the bot's presence on startup.
+class Feedback(commands.Cog):
+	def __init__(self, client):
+		self.client = client
+	
+	@commands.Cog.listener()
+	async def on_ready(self):
+		self.channel = self.client.get_channel(utils.master_settings["feedback"])
+	
+	@commands.command()
+	async def feedback(self, ctx, *, arg):
+		await self.channel.send(arg)
+
+def setup(client):
+	client.add_cog(Feedback(client))
