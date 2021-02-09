7742dfb2ef2d2fbf4938c1df1674c42410c2a4f9
Camto <www.ben.pr@gmail.com>
2021-02-08 21:00:12 -0400

    Add some quips for if you're too slow.

--- Cogs/joke.py
+++ Cogs/joke.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import utils
 
 import asyncio
+import random
 
 class Joke(commands.Cog):
 	def __init__(self, client):
@@ -16,12 +17,12 @@ class Joke(commands.Cog):
 		await slow_send(channel, "Who's there?", 0.5)
 		msg = await utils.wait_for_response(self.client, channel, user, 60)
 		if msg is None:
-			return await ctx.send("no person")
+			return await ctx.send(random.choice(["Must've been the wind", "Guess there's nobody", "Eh, I'll just go back to bed"]))
 		
 		await slow_send(channel, f"{msg.content} who?", 0.5)
 		msg = await utils.wait_for_response(self.client, channel, user, 60)
 		if msg is None:
-			return await ctx.send("no punchline")
+			return await ctx.send(random.choice(["Was there really no punchline?", "Huh, where'd you go?", "Oh well, I was hoping for a punchline."]))
 		
 		await slow_send(channel, ":joy:", 0.5)
 
